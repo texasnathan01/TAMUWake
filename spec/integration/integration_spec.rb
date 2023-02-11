d7b949af59116e5c3cce91b4050e06b4f4d558a0
ChristopherPasala <christopher.pasala@gmail.com>
2023-02-11 16:19:14 -0600

    fixed path issue in integration testing

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -13,12 +13,12 @@ end
 
 RSpec.describe 'Creating a user', type: :feature do
   scenario 'valid inputs' do
-    visit new_receipt_path
+    visit new_user_path
     fill_in "user[firstname]", with: 'team'
     fill_in "user[lastname]", with: 'team'
     fill_in "user[email]", with: 'team'
-    click_on 'Create Receipt'
-    visit receipts_path
+    click_on 'Create User'
+    visit users_path
     expect(page).to have_content('team')
   end
 end
