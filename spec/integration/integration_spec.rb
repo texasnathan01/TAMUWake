22bffe07fdaa6e070b2c0a44b14b214c64f2e333
ChristopherPasala <christopher.pasala@gmail.com>
2023-02-11 16:04:00 -0600

    added unit and integration test

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -10,3 +10,15 @@ RSpec.describe 'Creating a receipt', type: :feature do
     expect(page).to have_content('teamteam')
   end
 end
+
+RSpec.describe 'Creating a user', type: :feature do
+  scenario 'valid inputs' do
+    visit new_receipt_path
+    fill_in "user[firstname]", with: 'team'
+    fill_in "user[lastname]", with: 'team'
+    fill_in "user[email]", with: 'team'
+    click_on 'Create Receipt'
+    visit receipts_path
+    expect(page).to have_content('team')
+  end
+end
