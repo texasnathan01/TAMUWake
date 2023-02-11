f67d32f91a676d2301e349b6feb2bba5e5c23ab9
Axel Ramone <axel.ramone@gmail.com>
2023-02-11 12:07:02 -0600

    UPDATED: validation on sets

--- app/models/wakeboard_set.rb
+++ app/models/wakeboard_set.rb
@@ -1,2 +1,11 @@
 class WakeboardSet < ApplicationRecord
+    validates :rider_limit, presence: true, numericality: { only_integer: true, greater_than: 0 }
+    validates :scheduled_date, presence: true, not_in_past: true
+    validate :current_rider_under_limit, on: :update
+
+    def current_rider_under_limit
+        if current_rider_count > rider_limit
+            errors.add(:current_rider_count, "exceeds the rider limit")
+        end
+    end
 end
