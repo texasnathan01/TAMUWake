be4d990205cd3b4ee1bd9ec6fe2c55a163be9036
Christopher Pasala <chrispasala@tamu.edu>
2023-03-06 13:20:05 -0600

    OAuth integration and testing (#39)
    * added page redirection
    
    * added integration tests

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -1,24 +1,75 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
+
+
 RSpec.describe 'Creating a receipt', type: :feature do
+  let(:admin) {Admin.create(email: "chrispasala@tamu.edu")}
+  let(:user) {User.create(email: "chrispasala@tamu.edu", firstname: "first", lastname: "last",role_id: 3)}
+
+  before :each do
+    allow_any_instance_of(ApplicationController).to receive(:authenticate_admin!).and_return(true)
+    allow_any_instance_of(ApplicationController).to receive(:current_admin).and_return(admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+
   scenario 'valid inputs' do
     visit new_receipt_path
-    fill_in "receipt[user_id]", with: 123
+    fill_in "receipt[value]", with: 123
     click_on 'Create Receipt'
     visit receipts_path
-    expect(page).to have_content('Receipt')
+    expect(page).to have_content('Receipt ID')
   end
 end
 
-RSpec.describe 'Creating a user', type: :feature do
-  scenario 'valid inputs' do
-    visit new_user_path
-    fill_in "user[firstname]", with: 'team'
-    fill_in "user[lastname]", with: 'team'
-    fill_in "user[email]", with: 'team'
-    click_on 'Create User'
-    visit users_path
-    expect(page).to have_content('team')
+RSpec.describe 'Account Pages', type: :feature do
+  let(:admin) {Admin.create(email: "chrispasala@tamu.edu")}
+  let(:user) {User.create(email: "chrispasala@tamu.edu", firstname: "first", lastname: "last",role_id: 1)}
+
+  before :each do
+    allow_any_instance_of(ApplicationController).to receive(:authenticate_admin!).and_return(true)
+    allow_any_instance_of(ApplicationController).to receive(:current_admin).and_return(admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
   end
+
+  scenario 'visiting account page ' do
+    visit accounts_path
+    expect(page).to have_content('Email:')
+  end
+end
+
+RSpec.describe 'Member Pages Without Access', type: :feature do
+
+  let(:admin) {Admin.create(email: "chrispasala@tamu.edu")}
+  let(:user) {User.create(email: "chrispasala@tamu.edu", firstname: "first", lastname: "last",role_id: -1)}
+  
+  before :each do
+    allow_any_instance_of(ApplicationController).to receive(:authenticate_admin!).and_return(true)
+    allow_any_instance_of(ApplicationController).to receive(:current_admin).and_return(admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+
+  scenario 'visiting member page without sufficient permissions' do
+    visit users_path
+    expect(page).to have_content('You do not have access')
+  end
+end
+
+
+RSpec.describe 'Member Pages With Access', type: :feature do
+
+  let(:admin) {Admin.create(email: "chrispasala@tamu.edu")}
+  let(:user) {User.create(email: "chrispasala@tamu.edu", firstname: "first", lastname: "last",role_id: 3)}
+
+  before :each do
+    allow_any_instance_of(ApplicationController).to receive(:authenticate_admin!).and_return(true)
+    allow_any_instance_of(ApplicationController).to receive(:current_admin).and_return(admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+
+  scenario 'visiting member page with sufficient permissions' do
+    visit users_path
+    expect(page).to have_content('Users')
+  end
+
 end
