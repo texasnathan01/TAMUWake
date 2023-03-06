b045997829d105efc998f63eb75f5cb94673e431
raistlynn <raistlynniyn@tamu.edu>
2023-03-05 21:08:36 -0600

    Added set_drivers
    Added set_drivers to the schema.
    Added code to automatically add creator of set as a driver of the set, but still need to work on adding a second driver and correctly counting the drivers on the set.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_202335) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_020939) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_202335) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_riders_on_user_id"
+  end
+
+  create_table "set_drivers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "rider_id", null: false
+    t.bigint "wakeboard_set_id"
   end
 
   create_table "set_riders", force: :cascade do |t|
