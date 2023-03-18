af785cd1725c7fbf6ca06fa509bfdd9c7d676e2c
Axel Ramone <axel.ramone@gmail.com>
2023-03-17 23:00:48 -0500

    FIXED: current_user used instead of current_admin

--- app/controllers/wakeboard_sets_controller.rb
+++ app/controllers/wakeboard_sets_controller.rb
@@ -13,7 +13,7 @@ class WakeboardSetsController < ApplicationController
 
   # GET /wakeboard_sets/1 or /wakeboard_sets/1.json
   def show
-    @joinable = !SetRider.rider_exists?(current_admin.id, params[:id])
+    @joinable = !SetRider.rider_exists?(current_user.id, params[:id])
     @riders = SetRider.where("wakeboard_set_id = ?", params[:id]).joins(:user).select(:firstname, :lastname, :as_dib)
   end
 
@@ -71,7 +71,7 @@ class WakeboardSetsController < ApplicationController
   def join
     @wakeboard_set = WakeboardSet.find(params[:id])
     as_dib = ActiveModel::Type::Boolean.new.cast(params[:as_dib])
-    user = current_admin.id
+    user = current_user.id
 
     respond_to do |format|
       if !@wakeboard_set.join(user, as_dib)
