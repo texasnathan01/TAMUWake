96c5fd08588b4fe3d660aeb987411ccae067c2cb
Axel Ramone <axel.ramone@gmail.com>
2023-04-01 19:10:25 -0500

    FIX: revert #55 for main

--- db/migrate/20230330023412_fix_user_relation_on_set_driver.rb
+++
@@ -1,6 +0,0 @@
-class FixUserRelationOnSetDriver < ActiveRecord::Migration[7.0]
-  def change
-    remove_reference :set_drivers, :rider
-    add_reference :set_drivers, :admin, foreign_key: true
-  end
-end
