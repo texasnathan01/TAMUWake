6c65d4aa2b1920a6604833f453daf4585fa34716
ChristopherPasala <christopher.pasala@gmail.com>
2023-04-01 19:51:34 -0500

    approval check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_admin!
+    append_before_action :check_user_approval
+
+    private
+
+    def check_user_approval
+        if current_admin && current_admin.is_approved?
+            flash[:alert] = "Your coount is not yet approved yet. Please wait for approval or contact your Administrators."
+            redirect_to root_path
+        end 
+    end
 end
