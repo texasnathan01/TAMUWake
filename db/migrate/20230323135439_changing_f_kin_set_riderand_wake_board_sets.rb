72ce151e6e32d7f71683b141265882bef533bef9
ChristopherPasala <christopher.pasala@gmail.com>
2023-04-08 23:35:33 -0500

    removed rider

--- db/migrate/20230323135439_changing_f_kin_set_riderand_wake_board_sets.rb
+++ db/migrate/20230323135439_changing_f_kin_set_riderand_wake_board_sets.rb
@@ -1,6 +1,5 @@
 class ChangingFKinSetRiderandWakeBoardSets < ActiveRecord::Migration[7.0]
   def change
-    add_reference :set_riders, :admin,  index: true, foreign_key: true
     remove_reference :wakeboard_sets, :admin, foreign_key: true
     add_column :admins, :role_id, :int, :default => 0
   end
