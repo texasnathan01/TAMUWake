ba24ea232e86317f17efcdd0c1855d1daf0fe65d
Christopher Pasala <chrispasala@tamu.edu>
2023-04-08 23:43:34 -0500

    Jira 59 google o auth integration and user settup (#73)
    * started styling
    
    * fixed receipt issue
    
    * removed rider
    
    * restoring migration

--- spec/integration/account_integration_spec.rb
+++ spec/integration/account_integration_spec.rb
@@ -1,7 +1,7 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
-RSpec.describe('Using Account Page and Tools', type: :feature) do
+RSpec.describe('Using Account Page and Tools as a Driver', type: :feature) do
   let(:admin) { Admin.create!(email: "chrispasala@tamu.edu", first_name: "first", last_name: "last", is_approved: true, role_id: -2) }
 
   before do
@@ -9,21 +9,34 @@ RSpec.describe('Using Account Page and Tools', type: :feature) do
     allow_any_instance_of(ApplicationController).to(receive(:current_admin).and_return(admin))
   end
 
-  it 'visiting account page' do
+  it 'and visiting account page' do
     visit accounts_path
     expect(page).to(have_content('chrispasala@tamu.edu'))
   end
 
-  it 'visiting account page and editing user info' do
+  it 'and visiting account page and editing user info' do
     visit edit_admin_path(admin.id)
     fill_in "admin[first_name]", with: "new_name"
     click_on 'Update Account Info'
     expect(page).to(have_content('new_name'))
   end
 
-  it 'visiting account page and editing user info without being approved' do
+  it 'and visiting account page and editing user info without being approved' do
     admin.is_approved = false
     visit edit_admin_path(admin.id)
     expect(page).to(have_content('not approved yet'))
   end
+
+  it 'and visiting edit page' do
+    visit edit_admin_path(admin.id)
+    expect(page).to(have_content('Editing Account Info'))
+  end
+
+  it 'and visiting edit page as a driver' do
+    visit edit_admin_path(admin.id)
+    expect(page).not_to(have_content('Are Boat Waiver Completed'))
+  end
+
+  
 end
+
