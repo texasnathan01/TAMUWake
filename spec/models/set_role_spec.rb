d368e0d775b66745e01a488e77a7c3718611837f
vj2139 <vj2139@tamu.edu>
2023-04-08 19:48:58 -0500

    made some merge conflict changes

--- spec/models/set_role_spec.rb
+++ spec/models/set_role_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
-<<<<<<< HEAD
-RSpec.describe SetRole, type: :model do
-=======
 RSpec.describe(SetRole) do
->>>>>>> origin/JIRA-59-Google-OAuth-Integration-and-User-Settup
   pending "add some examples to (or delete) #{__FILE__}"
 end
