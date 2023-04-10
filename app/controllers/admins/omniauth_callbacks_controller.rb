9c8c9d56eab15307b8b9fe0251b60cb80d5be445
Christopher Pasala <chrispasala@tamu.edu>
2023-04-10 17:05:01 -0500

    Chris css (#84)
    * css changes
    
    * added default admin
    
    * fixed role based access
    
    * styling for search and approval btns
    
    * added more css
    
    * added css to approval page
    
    * flexed table
    
    * working on fixing table
    
    * FIXED: table overflow issues
    
    ---------
    
    Co-authored-by: Axel Ramone <axel.ramone@gmail.com>

--- app/controllers/admins/omniauth_callbacks_controller.rb
+++ app/controllers/admins/omniauth_callbacks_controller.rb
@@ -10,11 +10,19 @@ class Admins::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       end
     end
     if admin.present?
+      #needed for admin account NOTE: important that this account is protected via admins and not shared to those without permission
+      if admin.email == "tamuwakeuser@gmail.com"
+        #gives admin account all roles on login
+        Role.all.map.each do |role|
+          puts role.role_name
+          admin.add_role(role.id)
+        end
+      end
       sign_out_all_scopes
       flash[:success] = t('devise.omniauth_callbacks.success', kind: 'Google')
       sign_in_and_redirect(admin, event: :authentication)
     else
-      flash[:alert] = t('devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized.")
+      flash[:alert] = t('devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized or not a TAMU account.")
       redirect_to(new_admin_session_path)
     end
   end
