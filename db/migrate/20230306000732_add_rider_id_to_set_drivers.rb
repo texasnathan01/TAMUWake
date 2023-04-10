4e2b074b780486aff69e47eed1bd48e888869f66
Axel <aramone@tamu.edu>
2023-04-09 21:21:59 -0500

    Set UI Fixes + Page Titles + Conditional Rendering (#78)
    * UPDATED: set ui for index and show
    
    * REMOVED: static ui elements + ADDED: rendering for null/empty items
    
    * UPDATED: ui + new has_role? method
    
    * ADDED: titles + role based rendering
    
    * FIX: commented out migrations that affect rspec runs

--- db/migrate/20230306000732_add_rider_id_to_set_drivers.rb
+++ db/migrate/20230306000732_add_rider_id_to_set_drivers.rb
@@ -1,5 +1,5 @@
 class AddRiderIdToSetDrivers < ActiveRecord::Migration[7.0]
   def change
-    add_column :set_drivers, :rider_id, :bigint, null:false
+    #add_column :set_drivers, :rider_id, :bigint, null:false
   end
 end
