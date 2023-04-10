8f11198265522b8e464eeb234a2fcf769c5eb7c9
Axel Ramone <axel.ramone@gmail.com>
2023-04-09 19:40:39 -0500

    UPDATED: ui + new has_role? method

--- app/helpers/wakeboard_sets_helper.rb
+++ app/helpers/wakeboard_sets_helper.rb
@@ -4,12 +4,17 @@ module WakeboardSetsHelper
   def set_available?(user_id, set, current_date = DateTime.current)
     if current_date.sunday.today?
       # sunday registration opens at 8pm
+      registration = DateTime.new(current_date.year, current_date.month, current_date.day, 20, 0, 0, current_date.zone)
+      if set.scheduled_date.in_time_zone.before?(registration)
+        return true
+      end
+
+      # set is after registration time
       if !WakeboardSet.sunday_registration_open?(set.scheduled_date, current_date)
         return false
       end
 
       # otherwise, registration is open but only 1 set can be joined by the user
-      registration = DateTime.new(current_date.year, current_date.month, current_date.day, 20, 0, 0)
       lower, upper = WakeboardSet.available_set_range(registration)
       return !SetRider.rider_joined_between?(user_id, lower, upper)
 
