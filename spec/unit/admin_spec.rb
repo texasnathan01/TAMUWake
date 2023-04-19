2c045b6faf16e6bcc3c9947cdc3c459624c49634
ChristopherPasala <christopher.pasala@gmail.com>
2023-04-18 22:52:19 -0500

    clean up

--- spec/unit/admin_spec.rb
+++ spec/unit/admin_spec.rb
@@ -1,12 +1,10 @@
 # location: spec/unit/admin_spec.rb
 require 'rails_helper'
 
-RSpec.describe Admin, type: :model do
-  subject do
-    described_class.new(email: "chrispasala@tamu.edu" ,first_name: 'Chris',last_name: 'Pasala')
-  end
+RSpec.describe "a user" do
 
-  it 'is valid with valid attributes' do
+  it "has their information using a helper" do
+    user = 
     expect(subject).to be_valid
   end
 
@@ -14,16 +12,5 @@ RSpec.describe Admin, type: :model do
     subject.first_name = "Axel"
     expect(subject).to be_valid
   end
-
-  describe 'role helper methods - rider_can_join' do
-    before :each do
-      @user = Admin.find_or_create_by(email: "aramone@tamu.edu")
-      @set = WakeboardSet.create(
-        dib_count: 0,
-        chib_count: 0,
-        driver_count: 1,
-        scheduled_date: (DateTime.current).tomorrow
-      )
-    end
 
 end
