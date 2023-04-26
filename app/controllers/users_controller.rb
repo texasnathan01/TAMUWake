1f0e1cf70e291d70bb72a9ed07f911dd1633ff74
ChristopherPasala <christopher.pasala@gmail.com>
2023-04-25 21:24:03 -0500

    clean up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,23 +43,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1 or /users/1.json
-  def destroy
-    # need to check the role of the user to allow for deletion
-    if @user.has_role?("Admin")
-      @user.destroy
-      respond_to do |format|
-        format.html { redirect_to(users_url, notice: "User was successfully destroyed.") }
-        format.json { head(:no_content) }
-      end
-    else
-      # redirect to account if they do not have permission
-      respond_to do |format|
-        format.html { redirect_to(accounts_url, notice: "You do not have access to this page. Contact your adminstrator for help.") }
-      end
-    end
-  end
-
   private
 
     # Use callbacks to share common setup or constraints between actions.
